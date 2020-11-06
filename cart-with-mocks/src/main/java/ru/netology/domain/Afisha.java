e0b864ce28a8010c6fb36619f6efc645af048d8a
rimmkin <rim2290@mail.ru>
2020-11-06 17:59:59 +0300

    mockito

--- cart-with-mocks/src/main/java/ru/netology/domain/Afisha.java
+++ cart-with-mocks/src/main/java/ru/netology/domain/Afisha.java
@@ -1,9 +1,13 @@
 package ru.netology.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
-public class AfishaMovieItem {
+@Data
+public class Afisha {
   private int id;
   private int filmId;
   private String filmName;
